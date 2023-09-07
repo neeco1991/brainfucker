58b30cd91029d6b464f7f99ea8c2a500fba36dc6
Nicola Scionti <nicolascionti@gmail.com>
2023-09-07 17:17:33 +0200

    riddles routes

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,11 +4,15 @@ import { supportedLanguages } from '../lib/lang';
 // 2. Define your collection(s)
 const riddlesCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    difficulty: z.enum(['ez', 'moderate', 'intense', 'brainfucker']),
-    lang: z.enum(supportedLanguages.map(({ id }) => id) as [string, ...string[]])
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      difficulty: z.enum(['ez', 'moderate', 'intense', 'brainfucker']),
+      lang: z.enum(
+        supportedLanguages.map(({ id }) => id) as [string, ...string[]]
+      ),
+      image: image(),
+    }),
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
