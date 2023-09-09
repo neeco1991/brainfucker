858f33de8e6cbaac2ee3ea1671680a2a3c44c82b
Nicola Scionti <nicolascionti@gmail.com>
2023-09-09 18:25:54 +0200

    update and riddle pages init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
+import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), mdx()]
+  integrations: [tailwind(), solidJs(), mdx()],
+  markdown: {},
 });
--- src/components/DifficultyBadge.tsx
+++ src/components/DifficultyBadge.tsx
@@ -8,25 +8,25 @@ export default function DifficultyBadge({
   switch (difficulty) {
     case 'brainfucker':
       return (
-        <span class="inline-flex font-rubik items-center rounded-md bg-brainfucker/10 px-2 py-1 text-brainfucker ring-1 ring-inset ring-brainfucker/20">
+        <span class="inline-flex font-rubik items-center rounded-md bg-brainfucker/30 px-2 py-1 text-brainfucker ring-1 ring-inset ring-brainfucker/20">
           Brainfucker
         </span>
       );
     case 'intense':
       return (
-        <span class="inline-flex font-rubik items-center rounded-md bg-intense/10 px-2 py-1 text-intense ring-1 ring-inset ring-intense/20">
+        <span class="inline-flex font-rubik items-center rounded-md bg-intense/30 px-2 py-1 text-intense ring-1 ring-inset ring-intense/20">
           Intense
         </span>
       );
     case 'moderate':
       return (
-        <span class="inline-flex font-rubik items-center rounded-md bg-moderate/10 px-2 py-1 text-moderate ring-1 ring-inset ring-moderate/20">
+        <span class="inline-flex font-rubik items-center rounded-md bg-moderate/30 px-2 py-1 text-moderate ring-1 ring-inset ring-moderate/20">
           Moderate
         </span>
       );
     case 'ez':
       return (
-        <span class="inline-flex font-rubik items-center rounded-md bg-ez/10 px-2 py-1 text-ez ring-1 ring-inset ring-ez/20">
+        <span class="inline-flex font-rubik items-center rounded-md bg-ez/30 px-2 py-1 text-ez ring-1 ring-inset ring-ez/20">
           Ez
         </span>
       );
